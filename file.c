3d1983cb89239c96ada35c100c303da1ae3711a8
HTheophile <125271494+HTheophile@users.noreply.github.com>
2023-03-22 17:03:53 +0100

    Add files via upload

--- file.c
+++ file.c
@@ -13,26 +13,22 @@ if (fileVide(ptrF))
     {
     return 0;
     }
-    *ptrE= ptrF->Elts[ptrF->Tete];
+    if (ptrF->Tete==MAX-1)
+        ptrF->Tete=-1;
     ptrF->Tete++;
-    if (ptrF->Tete==MAX)
-    {
-        ptrF->Tete=0;
-    }
+    affecterElt(ptrE, &(ptrF->Elts[ptrF->Tete]));
 return 1;
 }
 
 int ajouter(T_File *ptrF,T_Elt *ptrE) // si espace libre, ajout en queue
 {
-    if (filePleine(ptrF))
-        return 0;
-    ptrF->Queue++;
-    if (ptrF->Queue==MAX)
-        ptrF->Queue=0;
-    ptrF->Elts[ptrF->Queue]= *ptrE;
-    if (ptrF->Tete ==-1){
-        ptrF->Tete=0;
+    if (filePleine(ptrF)){
+    return 0;
     }
+    if (ptrF->Queue==MAX-1)
+        ptrF->Queue=-1;
+    ptrF->Queue++;
+    affecterElt(&(ptrF->Elts[ptrF->Queue]),ptrE);
 return 1;
 } 
 
@@ -47,7 +43,7 @@ int filePleine(const  T_File *ptrF) // qd MAX elts dans la file
 {
     if (ptrF->Queue>ptrF->Tete)
     {
-        if (ptrF->Queue+ptrF->Tete==9)
+        if (ptrF->Queue+ptrF->Tete==8)
             return 1;
         return 0;
     }
@@ -61,19 +57,25 @@ int filePleine(const  T_File *ptrF) // qd MAX elts dans la file
 
 T_Elt  premier(T_File *ptrF) //valeur en tete de file
 {
+    if (ptrF->Tete==-1){
+        return  ptrF->Elts[ptrF->Tete+1];
+    }
 return  ptrF->Elts[ptrF->Tete];
 }
 
 void afficherFile(T_File *ptrF)
 {
     int i; 
-    for (i=ptrF->Tete; i != ptrF->Queue;i++)
+    i= ptrF->Tete;
+    while(i!= ptrF->Queue)
     {
-        if (i==MAX)
-        i=0;
+        i++;
         afficherElt(&(ptrF->Elts[i]));
+        if ((i==MAX-1) && (MAX-1 != ptrF->Queue))
+            i=-1;
+
     }
-    
+
 }
 
 
